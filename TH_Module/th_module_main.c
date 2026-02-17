6e2b2a2356b417fbb51a95663b2091a3d7b14810
黛玉 <pwg12377@gmail.com>
2026-02-17 20:18:52 +0900

    main 분리

---
+++ TH_Module/th_module_main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <mqueue.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#include "common.h"
+#include "th_module.h"
+
+int main(void) {
+
+    if (th_module_init("192.168.0.20", 8887) != 0) {
+        printf("초기화 실패\n");
+        return 1;
+    }
+
+    mqd_t mq = mq_open(TH_QUEUE_NAME, O_WRONLY);
+    if (mq == (mqd_t)-1) {
+        perror("MQ 열기 실패");
+        return 1;
+    }
+
+    while (1) {
+        THModuleData d = th_module_read_once();
+
+        THMsg msg;
+        msg.temperature = d.temperature;
+        msg.humidity = d.humidity;
+        msg.error_code = d.error_code;
+        msg.sys_errno = d.sys_errno;
+
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        msg.ts_ms = (uint64_t)ts.tv_sec * 1000 +
+                    ts.tv_nsec / 1000000;
+
+        mq_send(mq, (char*)&msg, sizeof(msg), 0);
+
+        sleep(5);
+    }
+
+    return 0;
+}
