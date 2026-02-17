b03e5e8a5d3bf32326fd21cceac70a888f03bf9a
黛玉 <pwg12377@gmail.com>
2026-02-17 20:05:57 +0900

    메시지 구조체 생성

--- TH_Module/common.h
+++ TH_Module/common.h
@@ -2,3 +2,12 @@
 #include <stdint.h>
 
 #define TH_QUEUE_NAME "/th_sensor_q" //TODO POSIX MQ에 사용할 큐 이름
+
+// 메시지 구조체
+typedef struct {
+  float temperature;
+  float humidity;
+  int error_code;
+  int sys_errno;
+  unit64_t ts_ms; // ts
+} THMsg;
