e131e1e54afc60c2c22237874c5780a13aea8b42
黛玉 <pwg12377@gmail.com>
2026-02-17 20:48:41 +0900

    MQ 통로 이름 수정
    기존 명칭에서 수정했음

--- TH_Module/common.h
+++ TH_Module/common.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 
-#define TH_QUEUE_NAME "/th_sensor_q" //TODO POSIX MQ에 사용할 큐 이름
+#define TH_QUEUE_NAME "/mq_th" //TODO POSIX MQ에 사용할 큐 이름
 
 // 메시지 구조체
 typedef struct {
